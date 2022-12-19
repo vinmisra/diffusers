96a839070e682eb7f18e69159e50158c2f2a5410
Vinith Misra <vmisra@roblox.com>
2022-12-19 13:42:32 -0800

    shot in the dark with multi-instance training

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -79,27 +79,33 @@ def parse_args(input_args=None):
         type=str,
         default=None,
         required=True,
-        help="A folder containing the training data of instance images.",
+        help="A folder containing the training data of instance images. OR a folder containing subfolders of training images for each multiinstance.",
     )
     parser.add_argument(
         "--class_data_dir",
         type=str,
         default=None,
         required=False,
-        help="A folder containing the training data of class images.",
+        help="A folder containing the training data of class images. OR a folder containing subfolders of training data for each multiinstance's class images.",
     )
     parser.add_argument(
         "--instance_prompt",
         type=str,
         default=None,
         required=True,
-        help="The prompt with identifier specifying the instance",
+        help="The prompt with identifier specifying the instance. OR a '\%\%'-separated list of such prompts for each instance.",
     )
     parser.add_argument(
         "--class_prompt",
         type=str,
         default=None,
-        help="The prompt to specify images in the same class as provided instance images.",
+        help="The prompt to specify images in the same class as provided instance images. OR a '\%\%'-separated list of such prompts for each instance.",
+    )
+    parser.add_argument(
+        "--multi_instance",
+        default=False,
+        action="store_true",
+        help="Flag to look for multiple instance examples, multiple instance prompts, multiple class prompts, and storing class images in multiple folders.",
     )
     parser.add_argument(
         "--with_prior_preservation",
@@ -274,36 +280,84 @@ class DreamBoothDataset(Dataset):
 
     def __init__(
         self,
-        instance_data_root,
-        instance_prompt,
+        instance_data_root:str,
+        instance_prompt:str,
         tokenizer,
-        class_data_root=None,
-        class_prompt=None,
+        class_data_root:Optional[str]=None,
+        class_prompt:Optional[str]=None,
         size=512,
         center_crop=False,
+        multi_instance:bool=False
     ):
         self.size = size
         self.center_crop = center_crop
         self.tokenizer = tokenizer
+        self.multi_instance = multi_instance
+
+        if self.multi_instance:
+            assert(len(instance_prompt.split('\%\%'))==len(sorted(list(Path(instance_data_root).iterdir()))))
 
         self.instance_data_root = Path(instance_data_root)
         if not self.instance_data_root.exists():
             raise ValueError("Instance images root doesn't exists.")
 
-        self.instance_images_path = list(Path(instance_data_root).iterdir())
-        self.num_instance_images = len(self.instance_images_path)
-        self.instance_prompt = instance_prompt
-        self._length = self.num_instance_images
+        if self.multi_instance: 
+            #precompute lengths and other stats for each instance.
+            #order instances by folder name, and order images by image name. We assume prompts are ordered similarly, and we assume classdirs have the same names.
+            self.instance_images_path = []
+            self.class_images_path = []
+            lst_path_instance_dir = sorted(list(Path(instance_data_root).iterdir()))
 
-        if class_data_root is not None:
-            self.class_data_root = Path(class_data_root)
-            self.class_data_root.mkdir(parents=True, exist_ok=True)
-            self.class_images_path = list(self.class_data_root.iterdir())
-            self.num_class_images = len(self.class_images_path)
-            self._length = max(self.num_class_images, self.num_instance_images)
-            self.class_prompt = class_prompt
+            if class_data_root is not None:
+                lst_path_class_dir = sorted(list(Path(class_data_root).iterdir()))
+
+            for idx_instance,instance_image_dir in enumerate(lst_path_instance_dir):
+                lst_imagepath_instance = list(instance_image_dir.iterdir())
+
+                if class_data_root is not None:
+                    class_image_dir = lst_path_class_dir[idx_instance]
+                    lst_imagepath_class = list(class_image_dir.iterdir())
+                    n_samples = max(len(lst_imagepath_instance),len(lst_imagepath_class))
+                else:
+                    n_samples = len(lst_imagepath_instance)
+                
+                self.instance_images_path.extend(
+                    [
+                        lst_imagepath_instance[index % len(lst_imagepath_instance)] for index in range(n_samples)
+                    ]
+                )
+                if class_data_root is not None:
+                    self.class_images_path.extend(
+                        [
+                            lst_imagepath_class[index % len(lst_imagepath_class)] \
+                                for index in range(len(lst_imagepath_instance))
+                        ]
+                    )
+                
+                self.which_instance.extend([idx_instance]*n_samples)
+
+            self.lst_instance_prompts = instance_prompt.split('\%\%')
+            if class_data_root is not None:
+                self.lst_class_prompts = class_prompt.split('\%\%')
+            
+            self._length = len(self.instance_images_path)
         else:
-            self.class_data_root = None
+            self.instance_images_path = list(Path(instance_data_root).iterdir())
+            self.instance_prompt = instance_prompt
+
+            self.num_instance_images = len(self.instance_images_path)
+            self._length = self.num_instance_images
+
+            if class_data_root is not None:
+                self.class_data_root = Path(class_data_root)
+                self.class_data_root.mkdir(parents=True, exist_ok=True)
+
+                self.class_images_path = list(self.class_data_root.iterdir())
+                self.class_prompt = class_prompt
+                self.num_class_images = len(self.class_images_path)
+                self._length = max(self.num_class_images, self.num_instance_images)
+            else:
+                self.class_data_root = None
 
         self.image_transforms = transforms.Compose(
             [
@@ -319,9 +373,15 @@ class DreamBoothDataset(Dataset):
 
     def __getitem__(self, index):
         example = {}
-        instance_image = Image.open(self.instance_images_path[index % self.num_instance_images])
+        instance_image = Image.open(self.instance_images_path[index])
         if not instance_image.mode == "RGB":
             instance_image = instance_image.convert("RGB")
+
+        if self.multi_instance:
+            instance_prompt = self.lst_instance_prompt[self.which_instance[index]]
+        else:
+            instance_prompt = self.instance_prompt
+        
         example["instance_images"] = self.image_transforms(instance_image)
         example["instance_prompt_ids"] = self.tokenizer(
             self.instance_prompt,
@@ -332,12 +392,18 @@ class DreamBoothDataset(Dataset):
         ).input_ids
 
         if self.class_data_root:
-            class_image = Image.open(self.class_images_path[index % self.num_class_images])
+            class_image = Image.open(self.class_images_path[index])
             if not class_image.mode == "RGB":
                 class_image = class_image.convert("RGB")
+
+            if self.multi_instance:
+                class_prompt = self.lst_class_prompt[self.which_instance[index]]
+            else:
+                class_prompt = self.class_prompt
+
             example["class_images"] = self.image_transforms(class_image)
             example["class_prompt_ids"] = self.tokenizer(
-                self.class_prompt,
+                class_prompt,
                 truncation=True,
                 padding="max_length",
                 max_length=self.tokenizer.model_max_length,
@@ -395,6 +461,43 @@ def get_full_repo_name(model_id: str, organization: Optional[str] = None, token:
     else:
         return f"{organization}/{model_id}"
 
+def hydrate_class_folder(class_images_dir:Path, class_prompt:str, args, accelerator):
+    if not class_images_dir.exists():
+        class_images_dir.mkdir(parents=True)
+    cur_class_images = len(list(class_images_dir.iterdir()))
+
+    if cur_class_images < args.num_class_images:
+        torch_dtype = torch.float16 if accelerator.device.type == "cuda" else torch.float32
+        pipeline = DiffusionPipeline.from_pretrained(
+            args.pretrained_model_name_or_path,
+            torch_dtype=torch_dtype,
+            safety_checker=None,
+            revision=args.revision,
+        )
+        pipeline.set_progress_bar_config(disable=True)
+
+        num_new_images = args.num_class_images - cur_class_images
+        logger.info(f"Number of class images to sample: {num_new_images}.")
+
+        sample_dataset = PromptDataset(class_prompt, num_new_images)
+        sample_dataloader = torch.utils.data.DataLoader(sample_dataset, batch_size=args.sample_batch_size)
+
+        sample_dataloader = accelerator.prepare(sample_dataloader)
+        pipeline.to(accelerator.device)
+
+        for example in tqdm(
+            sample_dataloader, desc="Generating class images", disable=not accelerator.is_local_main_process
+        ):
+            images = pipeline(example["prompt"]).images
+
+            for i, image in enumerate(images):
+                hash_image = hashlib.sha1(image.tobytes()).hexdigest()
+                image_filename = class_images_dir / f"{example['index'][i] + cur_class_images}-{hash_image}.jpg"
+                image.save(image_filename)
+
+        del pipeline
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
 
 def main(args):
     logging_dir = Path(args.output_dir, args.logging_dir)
@@ -417,45 +520,26 @@ def main(args):
 
     if args.seed is not None:
         set_seed(args.seed)
+    
+    if args.multi_instance:
+        #validate args are in the right format, set up directories, etc.
+        instance_dir_parent = Path(args.instance_data_dir)
+        lst_instance_names = [x.name for x in list(instance_dir_parent.iterdir())]
 
-    if args.with_prior_preservation:
+        lst_instance_prompts = args.instance_prompt.split('\%\%')
+        lst_class_prompts = args.class_prompt.split('\%\%')
+
+        assert(len(lst_instance_names) == len(lst_class_prompts))
+        assert(len(lst_instance_names) == len(lst_instance_prompts))
+
+        class_dir_parent = Path(args.class_data_dir)
+        lst_class_images = []
+        for instance_name, class_prompt in zip(lst_instance_names, lst_class_prompts):
+            class_images_dir = class_dir_parent / instance_name
+            hydrate_class_folder(class_images_dir, class_prompt, args, accelerator)
+    elif args.with_prior_preservation:
         class_images_dir = Path(args.class_data_dir)
-        if not class_images_dir.exists():
-            class_images_dir.mkdir(parents=True)
-        cur_class_images = len(list(class_images_dir.iterdir()))
-
-        if cur_class_images < args.num_class_images:
-            torch_dtype = torch.float16 if accelerator.device.type == "cuda" else torch.float32
-            pipeline = DiffusionPipeline.from_pretrained(
-                args.pretrained_model_name_or_path,
-                torch_dtype=torch_dtype,
-                safety_checker=None,
-                revision=args.revision,
-            )
-            pipeline.set_progress_bar_config(disable=True)
-
-            num_new_images = args.num_class_images - cur_class_images
-            logger.info(f"Number of class images to sample: {num_new_images}.")
-
-            sample_dataset = PromptDataset(args.class_prompt, num_new_images)
-            sample_dataloader = torch.utils.data.DataLoader(sample_dataset, batch_size=args.sample_batch_size)
-
-            sample_dataloader = accelerator.prepare(sample_dataloader)
-            pipeline.to(accelerator.device)
-
-            for example in tqdm(
-                sample_dataloader, desc="Generating class images", disable=not accelerator.is_local_main_process
-            ):
-                images = pipeline(example["prompt"]).images
-
-                for i, image in enumerate(images):
-                    hash_image = hashlib.sha1(image.tobytes()).hexdigest()
-                    image_filename = class_images_dir / f"{example['index'][i] + cur_class_images}-{hash_image}.jpg"
-                    image.save(image_filename)
-
-            del pipeline
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
+        hydrate_class_folder(class_images_dir, args.class_prompt, args, accelerator)
 
     # Handle the repository creation
     if accelerator.is_main_process:
@@ -566,6 +650,7 @@ def main(args):
         tokenizer=tokenizer,
         size=args.resolution,
         center_crop=args.center_crop,
+        multi_instance=args.multi_instance
     )
 
     train_dataloader = torch.utils.data.DataLoader(
